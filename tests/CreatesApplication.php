790b3c14aa3e1acb7255e2988853638331af791e
fhebriantz <lutfi.febrianto@gmail.com>
2019-01-24 17:48:40 +0700

    w

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -8,7 +8,7 @@ trait CreatesApplication
 {
     /**
      * Creates the application.
-     *
+     *sss
      * @return \Illuminate\Foundation\Application
      */
     public function createApplication()
